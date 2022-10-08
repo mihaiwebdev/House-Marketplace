862dcbc9eb32d472f6b115748da4d6e260139185
mihaiwebdev <1mihaisava1@gmail.com>
2022-10-08 18:01:54 +0300

    geolocation fix

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -11,7 +11,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 function CreateListing() {
     // eslint-disable-next-line
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+    const [geolocationEnabled, setGeolocationEnabled] = useState(false);
     const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
         type: 'rent',
